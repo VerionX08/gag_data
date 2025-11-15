120335ced1b3c615f1e20b2306f92b6fc68c22e2
VerionX08 <huy88888oplm@gmail.com>
2025-11-15 19:32:12 +0700

    VerionX_GaG.lua

--- VerionX_GaG.lua
+++ VerionX_GaG.lua
@@ -30,7 +30,7 @@ btn.ScaleType = Enum.ScaleType.Fit
 
 -- Cooldown setup
 local canClick = true
-local cooldown = 0.15 -- giây giữa các click
+local cooldown = 0.08 -- giây giữa các click
 
 btn.MouseButton1Click:Connect(function()
     if canClick then
